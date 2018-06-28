f3c42baff17170f2f7658b5768a49a84470448ed
DESKTOP-EOLBM5D\ryo.sakamoto <r.sakamoto@tcmobile.jp>
2018-06-28 10:56:33 +0900

    login処理追加 テーブルはUser

---
+++ src/main/java/com/example/himaGatya/login/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.himaGatya.login;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    public User findByUsername(String username);
+
+    public User findByMailAddress(String mailAddress);
+
+}
