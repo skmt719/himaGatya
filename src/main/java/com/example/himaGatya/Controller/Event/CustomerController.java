b3168be32be08481f37cee8855cd312c0747b13c
yamada <hiroki.like.neko@gmail.com>
2018-06-29 17:28:40 +0900

    eventの編集と削除の実装：完了

--- src/main/java/com/example/himaGatya/Controller/Event/CustomerController.java
+++ src/main/java/com/example/himaGatya/Controller/Event/CustomerController.java
@@ -10,8 +10,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-import com.example.himaGatya.Controller.Event.EventsServiceImpl;
+import com.example.himaGatya.Controller.EventsForm;
 
 @Controller
 public class CustomerController {
@@ -32,7 +33,7 @@ public class CustomerController {
 
 	////////////////////////////////////////////////////////
 	//
-	// データ確認用テストコード
+	// データ確認用テストコード test.html
 	//
 	////////////////////////////////////////////////////////
 	// Certifications entity
@@ -50,52 +51,69 @@ public class CustomerController {
 		return "/test";
 	}
 
-
 	////////////////////////////////////////////////////////
 	//
-	// 管理者側イベント一覧画面 (index)
+	// 管理者側イベント一覧画面 	list
 	//
 	////////////////////////////////////////////////////////
-
+	// 一覧表示
 	@GetMapping("/admin")
-	public String eventsList(Model model) {
-		/*Events eventsForm = new Events();
-		model.addAttribute("events",eventsForm);*/
+	public String showEventsList(Model model ) {
+		List<Events> customers = eventServiceImpl.getEventsList();
+		model.addAttribute("customers", customers);
 		return "/event/index";
 	}
-	@PostMapping("/admin")
-	public String eventsList(@ModelAttribute Events eventsForm) {
-		// table に上書き
-		eventServiceImpl.saveEvents(eventsForm);
-		return "/event/index";
+
+	// イベントの新規作成 jump edit page
+	@PostMapping(value="/admin" , params="createNew")
+	public String newEventCreate() {
+		return "redirect:/admin/edit";
+	}
+
+	// イベントの編集 jump edit page
+	@PostMapping(value="/admin" , params="update")
+	public ModelAndView selectEventEdit(ModelAndView mav ,
+								Model model ,
+								@ModelAttribute Events events ,
+								@ModelAttribute EventsForm form ,
+								BindingResult bindingresult) {
+		events = eventServiceImpl.getEventsById( form.getSelectedEvents() );	// form からidを取得し、eventServiceImplにてイベント本体を取得
+
+		model.addAttribute("events" , events);
+		mav.setViewName("event/edit");
+		return mav;
+	}
+
+	// イベントの削除
+	@PostMapping(value="/admin" , params="delete")
+	public String selectEventDelete(@ModelAttribute EventsForm form, BindingResult bindingresult) {
+		eventServiceImpl.deleteEventById(form.getSelectedEvents());	// table の指定イベントを削除
+		return "redirect:/admin";
 	}
 
 	////////////////////////////////////////////////////////
 	//
-	// 管理者側イベント編集画面
+	// 管理者側イベント編集画面		edit
 	//
 	////////////////////////////////////////////////////////
 	// Get処理 画面表示　　二重に処理してしまう？
 	@GetMapping("/admin/edit")
-	public String eventsEdit(Model model) {
+	public String GetEventsEdit(Model model) {
 		Events eventsForm = new Events();
-		model.addAttribute("eventsForm",eventsForm);
+		model.addAttribute("events",eventsForm);
 		return "/event/edit";
 	}
 	// データ登録 save ボタン処理
 	@PostMapping(value="/admin/edit" , params="save")
-	public String eventsEdit(@ModelAttribute Events eventsForm , BindingResult bindingresult) {
-		// table に上書き
-		eventServiceImpl.saveEvents(eventsForm);
-		return "/event/index";
+	public String eventsEdit(@ModelAttribute Events events , BindingResult bindingresult) {
+
+		eventServiceImpl.saveEvents(events);	// table に上書き
+		return "redirect:/admin";
 	}
-	// データ削除
-/*	@PostMapping("/admin/list")
-	public String deleteData(@ModelAttribute Events eventsForm) {
-
-		eventServiceImpl.deleteEventById(eventsForm.getId());
-		return "/";
-	}*/
-
+	// back処理 list に遷移
+	@PostMapping(value="/admin/edit" , params="back")
+	public String jumpEventsList() {
+		return "redirect:/admin";
+	}
 
 }
